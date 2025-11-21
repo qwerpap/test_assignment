d5f07e4a31ad959d973580b2d4e1151fd55f003f
gggloriammmartin-jpg <maksim.khlebnikov@example.com>
2025-11-21 18:42:42 +0600

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_assignment/features/main_screen/view/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: MainScreen(),
+    );
   }
 }
